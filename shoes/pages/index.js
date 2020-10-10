f2c17e372ac8ec8306d7839ecc5c485be1f5c039
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-10 16:58:20 +0300

    add nav

--- shoes/pages/index.js
+++ shoes/pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Nav from '../components/nav';
 
 export default function Home() {
 	return (
-  )
+		<Nav />
+	)
 }

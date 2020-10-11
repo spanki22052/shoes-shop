d9768e535200885e701df52f924fd68b6e22ed36
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-11 15:10:34 +0300

    add some scss

--- shoes/pages/index.js
+++ shoes/pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head';
 import Header from '../components/nav';
 import Sidebar from '../components/sidebar';
+import Main from '../components/main';
 
 export default function Home() {
 	return (
 		<div className='wrapper'>
 			<Header />
+			<Main />
 			<Sidebar />
 		</div>
 	)

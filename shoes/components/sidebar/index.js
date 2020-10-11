81c63cc119a706b0f4f23e9e6ec62d4a51725c2e
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-11 18:26:24 +0300

    add some code

--- shoes/components/sidebar/index.js
+++ shoes/components/sidebar/index.js
@@ -6,7 +6,6 @@ const Sidebar = () => {
 	const menuState = useSelector(state => state.menu.menuState);
 	const dispatch = useDispatch();
 	const [choosenCategoty, setCategory] = useState('');
-	// const categorys = ["Мужчинам", "Женщинам", "Детям", "Информационная помощь"]
 	const categorys = [
 		{
 			category: "Мужчинам",
@@ -21,7 +20,7 @@ const Sidebar = () => {
 			subcategory: ['Все товары', 'Новинки', 'Распродажа', 'Обувь']
 		},
 		{
-			category: "Информаци и помощь",
+			category: "Информация и помощь",
 			subcategory: ['Доставка', 'Гарантия', 'Возврат', 'Таблица размеров']
 		}
 	]
@@ -32,14 +31,20 @@ const Sidebar = () => {
 				className={classes.sideItem}>
 				<div
 					onClick={() => { choosenCategoty === item.category ? setCategory('') : setCategory(item.category) }}
-					className={choosenCategoty !== item.category ? classes.sideItemTitle : classes.sideItemTitle + ' ' + classes.activeTitle}>
-					{item.category}
+					className={choosenCategoty !== item.category ? classes.sidetitle : classes.sidetitle + ' ' + classes.activeTitle}>
+					<div
+						className={classes.sideItemTitle}>
+						{item.category}
+					</div>
+					<div className={choosenCategoty !== item.category ? classes.sideicon : classes.sideicon + ' ' + classes.activeIcon}>
+
+					</div>
 				</div>
 				<div className={choosenCategoty !== item.category ? classes.sideItemContent : classes.sideItemContent + ' ' + classes.activeContent}>
 					<ul className={classes.sublist}>
-						{item.subcategory.map((item, id) => {
+						{item.subcategory.map((item, idx) => {
 							return (
-								<li key={id} className={classes.subitem}>
+								<li key={idx} className={classes.subitem}>
 									{item}
 								</li>
 							)
@@ -56,6 +61,30 @@ const Sidebar = () => {
 				classes.sidebar + " " + classes.active}>
 			<ul className={classes.sidelist}>
 				{categoryList}
+				<li className={classes.sideItem}>
+					<div className={classes.sidetitle}>
+						<div
+							className={classes.sideItemTitle}>
+							Новости
+					</div>
+					</div>
+				</li>
+				<li className={classes.sideItem}>
+					<div className={classes.sidetitle}>
+						<div
+							className={classes.sideItemTitle}>
+							Акции
+					</div>
+					</div>
+				</li>
+				<li className={classes.sideItem}>
+					<div className={classes.sidetitle}>
+						<div
+							className={classes.sideItemTitle}>
+							Адреса магазинов
+					</div>
+					</div>
+				</li>
 			</ul>
 		</div >
 	)

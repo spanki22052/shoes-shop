f825cb991083b2cd97003e4abbd8c5b8641089d7
spanki22052 <useful22052@gmail.com>
2020-10-13 19:04:00 +0300

    add adaptive to category page

--- shoes/components/catalog/index.js
+++ shoes/components/catalog/index.js
@@ -3,21 +3,21 @@ import styles from "../../styles/catalog.module.scss";
 export default () => {
   return (
     <div className={styles.wrapper}>
-      <h1>Каталог</h1>
+      <h1 className={styles.catalogText}>Каталог</h1>
 
       <div className={styles.photosHolder}>
-          <div className={styles.photoBlock}>
-              <img src='/man.jpg' alt='man' />
-              <h1>Мужчинам</h1>
-          </div>
-          <div className={styles.photoBlock}>
-              <img src='/woman.jpg' alt='man' />
-              <h1>Женщинам</h1>
-          </div>
-          <div className={styles.photoBlock}>
-              <img src='/kid.jpg' alt='man' />
-              <h1>Детям</h1>
-          </div>
+        <div className={styles.photoBlock}>
+          <img src="/man.jpg" alt="man" />
+          <h1>Мужчинам</h1>
+        </div>
+        <div className={styles.photoBlock}>
+          <img src="/woman.jpg" alt="man" />
+          <h1>Женщинам</h1>
+        </div>
+        <div className={styles.photoBlock}>
+          <img src="/kid.jpg" alt="man" />
+          <h1>Детям</h1>
+        </div>
       </div>
     </div>
   );

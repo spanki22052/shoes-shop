6f6290a07937dd083747bca18db552e2cca819cb
spanki22052 <useful22052@gmail.com>
2020-10-13 18:29:31 +0300

    add normal photos

--- shoes/components/catalog/index.js
+++ shoes/components/catalog/index.js
@@ -10,6 +10,14 @@ export default () => {
               <img src='/man.jpg' alt='man' />
               <h1>Мужчинам</h1>
           </div>
+          <div className={styles.photoBlock}>
+              <img src='/woman.jpg' alt='man' />
+              <h1>Женщинам</h1>
+          </div>
+          <div className={styles.photoBlock}>
+              <img src='/kid.jpg' alt='man' />
+              <h1>Детям</h1>
+          </div>
       </div>
     </div>
   );

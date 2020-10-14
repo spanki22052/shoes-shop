1c7f38a56ab59632f6262067602da957f45d8333
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-14 20:51:47 +0300

    complited footer

--- shoes/components/footer/index.js
+++ shoes/components/footer/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import classes from '../../styles/footer.module.scss';
 
 const Footer = () => {
+	const categorys = [
+		{
+			category: "О КОМПАНИИ",
+			subcategory: ['История компании', 'Контакты', 'Правовая информация', 'Вакансии']
+		},
+		{
+			category: "ПОКУПАТЕЛЯМ",
+			subcategory: ['Оплата', 'Доставка', 'Возврат', 'Гарантия', 'Обработка данных']
+		},
+		{
+			category: "Детям",
+			subcategory: ['Все товары', 'Новинки', 'Распродажа', 'Обувь']
+		},
+		{
+			category: "Информация и помощь",
+			subcategory: ['Доставка', 'Гарантия', 'Возврат', 'Таблица размеров']
+		}
+	]
 	return (
 		<footer className={classes.footer}>
 			<div className={classes.footer__content}>
@@ -32,27 +50,103 @@ const Footer = () => {
 					</div>
 					<div className={classes.social}>
 						<div className={classes.socialTitle}>
-							Подписывайтесь на нас в соц.сетях и следите за новостями и акциями
+							Социальные сети:
 						</div>
-						<div className={classes.socialIcons}>
-
-						</div>
+						<img src='./instagram.svg'></img>
 					</div>
 					<div className={classes.cards}>
 						<div className={classes.cardTitle}>
 							Мы принимаем
 						</div>
 						<div className={classes.cardcons}>
-
+							<img src='./visa.svg' />
+							<img src='./mastercard.svg' />
 						</div>
 					</div>
 				</div>
 				<div className={classes.info}>
+					<ul className={classes.category}>
+						<li className={classes.categoryItem}>
+							<div className={classes.categoryTitle}>
+								О КОМПАНИИ
+								<div className={classes.categoryIcon}>
+								</div>
+							</div>
+							<div className={classes.subBlock}>
+								<ul className={classes.subCategory}>
+									<li className={classes.subCategoryItem}>
+										История компании
+									</li>
+									<li className={classes.subCategoryItem}>
+										Контакты
+									</li>
+									<li className={classes.subCategoryItem}>
+										Правовая информация
+									</li>
+									<li className={classes.subCategoryItem}>
+										Вакансии
+									</li>
+								</ul>
+							</div>
+						</li>
+						<li className={classes.categoryItem}>
+							<div className={classes.categoryTitle}>
+								ПОКУПАТЕЛЯМ
+								<div className={classes.categoryIcon}>
 
+								</div>
+							</div>
+							<div className={classes.subBlock}>
+								<ul className={classes.subCategory}>
+									<li className={classes.subCategoryItem}>
+										Оплата
+									</li >
+									<li className={classes.subCategoryItem}>
+										Доставка
+									</li>
+									<li className={classes.subCategoryItem}>
+										Возврат
+									</li>
+									<li className={classes.subCategoryItem}>
+										Гарантия
+									</li>
+									<li className={classes.subCategoryItem}>
+										Обработка данных
+									</li>
+								</ul>
+							</div>
+						</li>
+						<li className={classes.categoryItem}>
+							<div className={classes.categoryTitle}>
+								ПАРТНЕРАМ
+								<div className={classes.categoryIcon}>
+
+								</div>
+							</div>
+							<div className={classes.subBlock}>
+								<ul className={classes.subCategory}>
+									<li className={classes.subCategoryItem}>
+										Поставщикам
+									</li>
+									<li className={classes.subCategoryItem}>
+										Стать партнером
+									</li>
+									<li className={classes.subCategoryItem}>
+										Рекламные материалы
+									</li>
+								</ul>
+							</div>
+						</li>
+					</ul>
 				</div>
 			</div>
 			<div className={classes.footer__end}>
-
+				<div className={classes.rules}>
+					© 2020 RALF RINGER. Все права защищены.
+				</div>
+				<div className={classes.dev}>
+					Разработка сайта: spanki, vgvsshi
+				</div>
 			</div>
 		</footer>
 	)

615913d6f10e9f372d3aeb7060c71d1472fa96b3
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-19 22:03:01 +0300

    add card reducer

--- shoes/modules/reducers/laodedData.js
+++ shoes/modules/reducers/laodedData.js
@@ -18,5 +18,37 @@ const dataReducer = (state = initialState, action) => {
 			};
 		default:
 			return state;
+		//card
+		case 'ITEM_ADD_TO_CARD':
+			const newItem = state.productList.find(item => item.id === action.payload);
+			if (state.cartItems.findIndex(item => item.id === newItem.id) !== -1) {
+				const clone = state.cartItems.find(item => item.id === newItem.id)
+				const newCart = state.cartItems.filter(item => item.id !== newItem.id);
+				clone.amount = clone.amount + 1
+				newCart.push(clone);
+				return {
+					...state,
+					cartItems: newCart
+				};
+			} else {
+				newItem.amount = 1;
+				return {
+					...state,
+					cartItems: [
+						...state.cartItems,
+						newItem
+					]
+				};
+			};
+		case 'ITEM_REMOVE_FROM_CARD':
+			const idx = action.payload;
+			const itemIndex = state.cartItems.findIndex(item => item.id === idx);
+			return {
+				...state,
+				cartItems: [
+					...state.cartItems.slice(0, itemIndex),
+					...state.cartItems.slice(itemIndex + 1)
+				]
+			};
 	}
 }

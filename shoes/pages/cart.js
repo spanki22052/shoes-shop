bce80fa745b39bcc9d1b46c3c990b2d19975ebb3
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-20 02:34:13 +0300

    add some scss

--- shoes/pages/cart.js
+++ shoes/pages/cart.js
@@ -3,10 +3,11 @@ import Header from '../components/nav';
 import Sidebar from '../components/sidebar';
 import Footer from '../components/footer';
 import CartPage from '../components/cart';
+import classes from '../styles/cart.module.scss';
 
 const Cart = () => {
 	return (
-		<div className='wrapper'>
+		<div className={classes.cartWrapper}>
 			<Header />
 			<Sidebar />
 			<CartPage />

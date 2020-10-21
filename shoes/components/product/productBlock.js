ea6abe5ea242785365335c7fd88667dfb2964a43
spanki22052 <useful22052@gmail.com>
2020-10-21 17:54:20 +0300

    shop block clciked

--- shoes/components/product/productBlock.js
+++ shoes/components/product/productBlock.js
@@ -1,36 +1,43 @@
-import React from 'react';
-import classes from '../../styles/prodBlock.module.scss';
+import React from "react";
+import Link from "next/link";
+import classes from "../../styles/prodBlock.module.scss";
 
 const ProdBlock = ({ product }) => {
-	return (
-		<div className={classes.product}>
-			<div className={classes.freeBlock}>
-
-			</div>
-			<div className={classes.prodImgBlock}>
-				<img src={product.img} className={classes.prodImg} />
-				<span className={product.new ? classes.prodBadge : classes.displayNone}>
-					Новинка
-				</span>
-			</div>
-			<div className={classes.prodInfo}>
-				<div className={classes.prodTitle}>
-					{product.title}
-				</div>
-				<div className={classes.prodPrice}>
-					{product.price} P
-				</div>
-				<ul className={classes.prodSizes}>
-					{product.sizes.map((item, id) => {
-						if (product.sizes[product.sizes.length - 1] === item) {
-							return (<li key={id} className={classes.prodSize}>{item}</li>)
-						}
-						return (<li key={id} className={classes.prodSize}>{item},</li>)
-					})}
-				</ul>
-			</div>
-		</div>
-	)
-}
+  return (
+    <Link href="/product/krossi">
+	<div className={classes.product}>
+        <div className={classes.freeBlock}></div>
+        <div className={classes.prodImgBlock}>
+          <img src={product.img} className={classes.prodImg} />
+          <span
+            className={product.new ? classes.prodBadge : classes.displayNone}
+          >
+            Новинка
+          </span>
+        </div>
+        <div className={classes.prodInfo}>
+          <div className={classes.prodTitle}>{product.title}</div>
+          <div className={classes.prodPrice}>{product.price} P</div>
+          <ul className={classes.prodSizes}>
+            {product.sizes.map((item, id) => {
+              if (product.sizes[product.sizes.length - 1] === item) {
+                return (
+                  <li key={id} className={classes.prodSize}>
+                    {item}
+                  </li>
+                );
+              }
+              return (
+                <li key={id} className={classes.prodSize}>
+                  {item},
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
 export default ProdBlock;

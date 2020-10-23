b839f5d5d3641af89db927f082a3c8102d0364eb
spanki22052 <useful22052@gmail.com>
2020-10-23 16:00:59 +0300

    adminpanel scss

--- shoes/components/adminpanel/index.js
+++ shoes/components/adminpanel/index.js
@@ -1,10 +1,13 @@
 import classes from "../../styles/adminpanel.module.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import firebase from "firebase";
+import AddCategory from "./addCategory";
 
 export default () => {
   const router = useRouter();
+  const [currentState, setState] = useState("");
+
   useEffect(() => {
     firebase.auth().onAuthStateChanged(function (user) {
       if (user) {
@@ -16,10 +19,13 @@ export default () => {
   }, []);
 
   return (
-    <>
-      <a style={{ position: "absolute", top: "110px"}} href="#" onClick={() => firebase.auth().signOut()}>
-        Log out
-      </a>{" "}
-    </>
+    <div className={classes.wrapper}>
+      <span className={classes.welcome}>Добро пожаловать</span>
+      <div className={classes.buttonsBlock}>
+        <button onClick={() => setState("1")}>Добавить категорию</button>
+        <button>Добавить продукт</button>
+      </div>
+      {currentState === "1" && <AddCategory />}
+    </div>
   );
 };

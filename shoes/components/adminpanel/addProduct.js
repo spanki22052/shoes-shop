132399b1700f57b38b7a5c815cb67b1da2e083a5
spanki22052 <useful22052@gmail.com>
2020-10-23 16:37:15 +0300

    add adminpanel buttons

---
+++ shoes/components/adminpanel/addProduct.js
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <>
+      <h1>product</h1>
+    </>
+  );
+};
--- shoes/components/adminpanel/index.js
+++ shoes/components/adminpanel/index.js
@@ -3,10 +3,11 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import firebase from "firebase";
 import AddCategory from "./addCategory";
+import AddProduct from "./addProduct";
 
 export default () => {
   const router = useRouter();
-  const [currentState, setState] = useState("");
+  const [currentState, setState] = useState("1");
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged(function (user) {
@@ -23,9 +24,10 @@ export default () => {
       <span className={classes.welcome}>Добро пожаловать</span>
       <div className={classes.buttonsBlock}>
         <button onClick={() => setState("1")}>Добавить категорию</button>
-        <button>Добавить продукт</button>
+        <button onClick={() => setState("2")}>Добавить продукт</button>
       </div>
       {currentState === "1" && <AddCategory />}
+      {currentState === "2" && <AddProduct />}
     </div>
   );
 };

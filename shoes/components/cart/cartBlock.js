0737ee141ed141c2a403c1a74770f958437cc51b
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-25 14:37:30 +0300

    remove sizes from cart

--- shoes/components/cart/cartBlock.js
+++ shoes/components/cart/cartBlock.js
@@ -8,8 +8,6 @@ import { deleteFromCard, incAmount, decAmount } from '../../modules/actions';
 const CartBlock = ({ prod }) => {
 
 	const dispatch = useDispatch();
-	const [itemSize, changeItemSize] = useState('');
-	const [choosenSize, changeSize] = useState(41);
 	const [amount, setAmount] = useState(1);
 	const currentPrice = prod.isSale ? prod.price - (prod.price * prod.sale) / 100 : prod.price;
 
@@ -35,38 +33,6 @@ const CartBlock = ({ prod }) => {
 				</div>
 			</div>
 			<div className={classes.prodInfo}>
-				<div
-					onClick={() => {
-						itemSize === prod.title ?
-							changeItemSize('') :
-							changeItemSize(prod.title)
-					}}
-					className={classes.sizeBlock}>
-					<div className={classes.prodSize}>
-						{choosenSize}
-						<svg
-							className={itemSize !== prod.title ? '' : classes.activeIcon}
-							width="20"
-							height="20"
-							viewBox="0 0 20 20"
-							fill="#b8b8b8"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path d="M15 14L10 9L5 14L3 13L10 6L17 13L15 14Z" fill="#b8b8b8" />
-						</svg>
-					</div>
-					<div className={itemSize !== prod.title ? classes.sizeSub : classes.sizeSub + ' ' + classes.active}>
-						{prod.sizes.map((item, id) => {
-							return (
-								<div
-									onClick={() => changeSize(item)}
-									key={id}
-									className={choosenSize !== item ? classes.size : classes.size + ' ' + classes.activeSize}
-								>{item}</div>
-							)
-						})}
-					</div>
-				</div>
 				<div className={classes.prodAmount}>
 					<span className={prod.amount === 1 ? '' : classes.active} onClick={() => { dispatch(decAmount(prod.productCode)); }}>-</span>
 					<div>{prod.amount}</div>

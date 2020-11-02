4050b0ba2f6571b03f12a1145612c27864a7a1e3
spanki22052 <useful22052@gmail.com>
2020-11-02 22:56:40 +0300

    productlist page blocks ready

--- shoes/components/product/productBlock.js
+++ shoes/components/product/productBlock.js
@@ -2,34 +2,42 @@ import React from "react";
 import Link from "next/link";
 import classes from "../../styles/prodBlock.module.scss";
 
-
 const ProdBlock = ({ product }) => {
-	return (
-		<Link href={'/product/' + product.productCode}>
-			<div className={classes.product}>
-				<div className={classes.freeBlock}></div>
-				<div className={classes.prodImgBlock}>
-					<img src={product.img[0]} className={classes.prodImg} />
-					<div className={classes.BadgeBlock}>
-						<span
-							className={product.new ? classes.prodBadgeNew : classes.displayNone}
-						>
-							Новинка
-          		</span>
-						<span
-							className={product.isSale ? classes.prodBadgeSale : classes.displayNone}
-						>
-							{product.sale}% скидка
-					</span>
-					</div>
-				</div>
-				<div className={classes.prodInfo}>
-					<div className={classes.prodTitle}>{product.title}</div>
-					<div className={classes.prodPrice}>{product.isSale ? product.price - product.price * (product.sale / 100) : product.price} P</div>
-				</div>
-			</div>
-		</Link>
-	);
+  return (
+    <Link href={"/product/" + product.productCode}>
+      <div className={classes.product}>
+        <div className={classes.freeBlock}></div>
+        <div className={classes.prodImgBlock}>
+          <img src={product.img[0]} className={classes.prodImg} />
+          <div className={classes.BadgeBlock}>
+            <span
+              className={
+                product.new ? classes.prodBadgeNew : classes.displayNone
+              }
+            >
+              Новинка
+            </span>
+            <span
+              className={
+                product.isSale ? classes.prodBadgeSale : classes.displayNone
+              }
+            >
+              {product.sale}% скидка
+            </span>
+          </div>
+        </div>
+        <div className={classes.prodInfo}>
+          <div className={classes.prodTitle}>{product.title}</div>
+          <div className={classes.prodPrice}>
+            {product.isSale
+              ? product.price - product.price * (product.sale / 100)
+              : product.price}{" "}
+            P <span>{product.isSale && product.price + "р"}</span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default ProdBlock;

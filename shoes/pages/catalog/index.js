065d10c3388faca0c03dd6576509a0f02fae690e
spanki22052 <useful22052@gmail.com>
2020-11-04 23:16:21 +0300

    catalog

--- shoes/pages/catalog/index.js
+++ shoes/pages/catalog/index.js
@@ -2,12 +2,17 @@ import Header from "../../components/nav";
 import Sidebar from "../../components/sidebar";
 import Catalog from "../../components/catalog";
 import Footer from "../../components/footer";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/catalog/sintetic");
+  }, []);
   return (
     <div className="wrapper">
       <Header />
-      <Catalog />
       <Sidebar />
       <Footer />
     </div>
